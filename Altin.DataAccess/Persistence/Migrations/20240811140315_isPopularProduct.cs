c393cf59cec40e8481b6e71576918be7b62cc26f
Yasar Can <38841941+mas963@users.noreply.github.com>
2024-08-11 20:46:56 +0300

    add: new migration

---
+++ Altin.DataAccess/Persistence/Migrations/20240811140315_isPopularProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Altin.DataAccess.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class isPopularProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPopular",
+                table: "Products",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPopular",
+                table: "Products");
+        }
+    }
+}
