1967b35e2bdf847dbe379fd2c418e57f6b7d9058
Yasar Can <38841941+mas963@users.noreply.github.com>
2024-08-15 22:19:58 +0300

    add: news controller

---
+++ Altin.Web/Controllers/NewsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Altin.Web.Controllers;
+
+[Route("Haberler")]
+public class NewsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
